7f7962d577de4643fd2faedc953eb3be032f2369
jack <jackev@protonmail.com>
2021-12-09 15:49:46 +0000

    Categorised JSON response

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import aiohttp
-from chia.cmds.farm_funcs import summary, get_blockchain_state, get_harvesters, is_farmer_running, get_wallets_stats, \
+from chia.cmds.farm_funcs import  get_blockchain_state, get_harvesters, is_farmer_running, get_wallets_stats, \
     get_average_block_time
 from chia.cmds.units import units
 from chia.consensus.block_record import BlockRecord
@@ -15,12 +15,16 @@ app = Flask(__name__)
 @app.route('/status')
 async def test():
 
-    await summary(None, None, None, None)
     all_harvesters = await get_harvesters(None)
     blockchain_state = await get_blockchain_state(None)
     farmer_running = await is_farmer_running(None)
 
     status = {}
+    status['plots'] = {}
+    status['network'] = {}
+    status['profit'] = {}
+    status['wins'] = {}
+
     if blockchain_state is None:
         status['state'] = "Not available"
     elif blockchain_state["sync"]["sync_mode"]:
@@ -64,8 +68,8 @@ async def test():
                 total_plot_size_harvester = sum(map(lambda x: x["file_size"], plots["plots"]))
                 PlotStats.total_plot_size += total_plot_size_harvester
                 PlotStats.total_plots += len(plots["plots"])
-                status['plots_count'] = len(plots['plots'])
-                status['plots_size'] = format_bytes(total_plot_size_harvester)
+                status['plots']['plots_count'] = len(plots['plots'])
+                status['plots']['plots_size'] = format_bytes(total_plot_size_harvester)
 
         if len(harvesters_local) > 0:
             print(f"Local Harvester{'s' if len(harvesters_local) > 1 else ''}")
@@ -84,14 +88,14 @@ async def test():
 
     if blockchain_state is not None:
         p: BlockRecord = blockchain_state['peak']
-        status['block_height'] = p.height
-        status['estimated_size'] = format_bytes(blockchain_state["space"])
+        status['network']['block_height'] = p.height
+        status['network']['estimated_size'] = format_bytes(blockchain_state["space"])
 
     if amounts is not None:
-        status['total_farmed'] = amounts['farmed_amount'] / units['chia']
-        status['tx_fees'] = amounts['fee_amount'] / units['chia']
-        status['block_rewards'] = (amounts['farmer_reward_amount'] + amounts['pool_reward_amount']) / units['chia']
-        status['last_farm_height'] = amounts['last_height_farmed']
+        status['wins']['total_farmed'] = amounts['farmed_amount'] / units['chia']
+        status['wins']['tx_fees'] = amounts['fee_amount'] / units['chia']
+        status['wins']['block_rewards'] = (amounts['farmer_reward_amount'] + amounts['pool_reward_amount']) / units['chia']
+        status['wins']['last_farm_height'] = amounts['last_height_farmed']
 
     minutes = -1
     if blockchain_state is not None and all_harvesters is not None:
@@ -99,16 +103,16 @@ async def test():
         minutes = int((await get_average_block_time(None) / 60) / proportion) if proportion else -1
 
     if all_harvesters is not None and PlotStats.total_plots == 0:
-        status['time_to_win'] = "Never"
-        status['time_to_win_days'] = "Never"
+        status['wins']['time_to_win'] = "Never"
+        status['wins']['time_to_win_days'] = "Never"
     else:
-        status['time_to_win'] = format_minutes(minutes)
-        status['time_to_win_days'] = int(minutes / (60 * 24))
+        status['wins']['time_to_win'] = format_minutes(minutes)
+        status['wins']['time_to_win_days'] = int(minutes / (60 * 24))
 
-    status['xch_price_usd'] = get_xch_price()
-    if status['time_to_win'] != "Never":
-        status['profit_daily'] = (2 * status['xch_price_usd']) / float(status['time_to_win_days'])
-        status['profit_30_days'] = status['profit_daily'] * 30
+    status['profit']['xch_price_usd'] = get_xch_price()
+    if status['wins']['time_to_win'] != "Never" and blockchain_state is not None:
+        status['profit']['profit_daily'] = (2 * status['profit']['xch_price_usd']) / float(status['wins']['time_to_win_days'])
+        status['profit']['profit_30_days'] = status['profit']['profit_daily'] * 30
 
     return jsonify(status)
 
