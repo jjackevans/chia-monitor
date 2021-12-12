4e7f43046f26879cef249f26f1a92d532bb94c0d
jack <jackev@protonmail.com>
2021-12-12 15:00:03 +0000

    Added partials line grab

--- monitor/log.py
+++ monitor/log.py
@@ -13,6 +13,10 @@ class Log:
     def get_latest_logs(self):
         for log_line in Pygtail(self.logfile, read_from_end=True):
             if "eligible for farming" in log_line:
-                print(log_line)
+                splitsville = log_line.split(" ")
+                plots_loc = splitsville.index("plots") - 1
+                proof_loc = splitsville.index("proofs.") - 1
+                time_loc = splitsville.index("Time:") + 1
+                print(splitsville[plots_loc], splitsville[proof_loc], splitsville[time_loc])
 
 
