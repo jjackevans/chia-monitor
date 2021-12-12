1b4506a619a7fbac404864b339a02d43e3794908
jack <jackev@protonmail.com>
2021-12-12 13:41:47 +0000

    Categorised JSON response

--- monitor/log.py
+++ monitor/log.py
@@ -4,9 +4,8 @@ from pygtail import Pygtail
 class Log:
 
     def __init__(self, logfile):
-        self.logfile = logfile
-        self.logfile = ""
-        self.latest_timestamp = ""
+        # self.logfile = logfile
+        self.logfile = "/root/.chia/mainnet/log/debug.log"
 
     def get_latest_logs(self):
         for log_line in Pygtail(self._expanded_log_path, read_from_end=True):
